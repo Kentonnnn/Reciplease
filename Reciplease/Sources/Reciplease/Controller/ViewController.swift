4bb7fb9c9a0b048ef87ba54de05a7a7c8805047d
Nelson PIRES DA SILVA <102812289+Kentonnnn@users.noreply.github.com>
2023-11-09 23:35:03 +0100

    architecture first commit

---
+++ Reciplease/Sources/Reciplease/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Nelson Pires Da Silva on 11/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
