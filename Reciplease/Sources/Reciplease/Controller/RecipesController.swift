3b041e993018d9d6e1bb50663eea3ef90091b9bf
Nelson PIRES DA SILVA <102812289+Kentonnnn@users.noreply.github.com>
2023-11-15 06:49:11 +0100

    btn search recipes done

---
+++ Reciplease/Sources/Reciplease/Controller/RecipesController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchForRecipesController.swift
+//  Reciplease
+//
+//  Created by Nelson Pires Da Silva on 11/15/23.
+//
+
+import UIKit
+
+class RecipesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
