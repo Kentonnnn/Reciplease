be80aeeacd5a776e6912b764aee499da5005dd73
Nelson PIRES DA SILVA <102812289+Kentonnnn@users.noreply.github.com>
2023-12-05 17:19:43 +0100

    add black rectangle recipe

--- Reciplease/Sources/Views/Recipe/CustomeRecipeCell.swift
+++ Reciplease/Sources/Views/Recipe/CustomeRecipeCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class CustomRecipeCell: UITableViewCell {
     
+    private lazy var blackRectangleView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    
     static var cellIdentifier = "RecipeCell"
     
     private let containerView = UIView()
@@ -33,29 +40,29 @@ class CustomRecipeCell: UITableViewCell {
     
     private func setupStyle() {
         self.containerView.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.97, alpha: 1)
-        //self.containerView.layer.cornerRadius = 10
         
-        //self.layer.cornerRadius = 10
         self.clipsToBounds = true
         
         self.recipeLabel.textColor = .white
         self.recipeLabel.font = UIFont.boldSystemFont(ofSize: 17)
-        self.recipeLabel.numberOfLines = 1                 // ça ne marche pas
-        self.recipeLabel.lineBreakMode = .byTruncatingTail    // ça ne marche pas
+        self.recipeLabel.numberOfLines = 1
+        self.recipeLabel.lineBreakMode = .byTruncatingTail
         
         self.recipeImageView.contentMode = .scaleAspectFill
         self.recipeImageView.clipsToBounds = true
     }
     
     func setupView() {
-        self.containerView.addSubview(recipeImageView)
-        self.containerView.addSubview(recipeLabel)
+        self.containerView.addSubview(self.recipeImageView)
+        self.containerView.addSubview(self.blackRectangleView)
+        self.containerView.addSubview(self.recipeLabel)
         
-        self.contentView.addSubview(containerView)
+        self.contentView.addSubview(self.containerView)
         
         self.containerView.translatesAutoresizingMaskIntoConstraints = false
         self.recipeLabel.translatesAutoresizingMaskIntoConstraints = false
         self.recipeImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.blackRectangleView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func setupLayout() {
@@ -70,9 +77,14 @@ class CustomRecipeCell: UITableViewCell {
             self.recipeImageView.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
             self.recipeImageView.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
             
+            self.blackRectangleView.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
+            self.blackRectangleView.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
+            self.blackRectangleView.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor),
+            self.blackRectangleView.heightAnchor.constraint(equalToConstant: 30),
+            
             self.recipeLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 10),
             self.recipeLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.containerView.trailingAnchor, constant: -10),
-            self.recipeLabel.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -10)
+            self.recipeLabel.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -5)
         ])
     }
 }
